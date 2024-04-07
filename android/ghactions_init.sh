e9c4d32b77fca36187b927d0fd4426797cbdc6af
kercre123 <kerigancreighton@gmail.com>
2024-04-06 21:09:01 -0500

    Make ghactions init script quieter

--- android/ghactions_init.sh
+++ android/ghactions_init.sh
@@ -7,15 +7,17 @@ echo $KEY | base64 -d > key/ks.jks
 echo $PASSWD > key/passwd
 
 if [[ ! -d android-ndk ]]; then
-    wget https://dl.google.com/android/repository/android-ndk-r23c-linux.zip
-    unzip android-ndk-r23c-linux.zip
+    echo "Getting android-ndk..."
+    wget -q https://dl.google.com/android/repository/android-ndk-r23c-linux.zip
+    unzip -qq android-ndk-r23c-linux.zip
     rm android-ndk-r23c-linux.zip
     mkdir android-ndk
     mv android-ndk-r23c android-ndk/ndk-bundle
 fi
 
 if [[ ! -d android-14 ]]; then
-    wget https://mirrors.cloud.tencent.com/AndroidSDK/build-tools_r34-linux.zip
-    unzip build-tools_r34-linux.zip
+    echo "Getting build-tools"
+    wget -q https://mirrors.cloud.tencent.com/AndroidSDK/build-tools_r34-linux.zip
+    unzip -qq build-tools_r34-linux.zip
     rm build-tools_r34-linux.zip
 fi
